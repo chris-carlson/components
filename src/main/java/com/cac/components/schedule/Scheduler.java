10a5003f96068dbc2b4b2901fa7f92eb9254800d
Chris Carlson <Chris Carlson>
2019-01-06 16:35:22 -0500

    Created scheduler

---
+++ src/main/java/com/cac/components/schedule/Scheduler.java
@@ -0,0 +1,25 @@
+package com.cac.components.schedule;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class Scheduler {
+    private ScheduledExecutorService rep;
+
+    public Scheduler() {
+        rep = Executors.newSingleThreadScheduledExecutor();
+    }
+
+    public void schedule(Runnable command, long delay, TimeUnit unit) {
+        rep.schedule(command, delay, unit);
+    }
+
+    public void scheduleAtFixedRate(Runnable command, long delay, long period, TimeUnit unit) {
+        rep.scheduleAtFixedRate(command, delay, period, unit);
+    }
+
+    public void scheduleWithFixedDelay(Runnable command, long delay, long period, TimeUnit unit) {
+        rep.scheduleWithFixedDelay(command, delay, period, unit);
+    }
+}
